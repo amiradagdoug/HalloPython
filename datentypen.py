76471870ec683e0e1d75ee08a2582895f7089e19
amira <amira.dagdoug@docc.techstarter.de>
2023-03-17 11:06:54 +0100

    jjjjjjjjj

---
+++ datentypen.py
@@ -0,0 +1,13 @@
+Kleiderschrank=["Hose", "T-Shirt", "Kleid"]
+Komode=["Schuhe", "Socken", "Muetzen"]
+print (Kleiderschrank)
+print(Kleiderschrank[0])
+print(Kleiderschrank[1])
+print(Kleiderschrank[2])
+for i in Kleiderschrank:
+ print ( i )
+Kleiderschrank.insert(3,"Pullover") 
+
+print(Kleiderschrank)
+Kleiderschrank.append(Komode)
+print(Kleiderschrank)
